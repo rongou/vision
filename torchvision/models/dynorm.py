a3187cf1a4406689fd4060309aef182a155187c0
Rong Ou <rong.ou@gmail.com>
2025-03-28 11:45:33 -0700

    better organize code

--- torchvision/models/dynorm.py
+++ torchvision/models/dynorm.py
@@ -4,72 +4,85 @@ import torch
 from torch import nn
 
 
-class DyT(nn.Module):
-    def __init__(self, num_features: int, init_alpha: float = 1.0) -> None:
+class AffineTransform(nn.Module):
+    def __init__(self, num_features: int) -> None:
         super().__init__()
-        self.alpha = nn.Parameter(torch.tensor(init_alpha))
         self.gamma = nn.Parameter(torch.ones(num_features))
         self.beta = nn.Parameter(torch.zeros(num_features))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         gamma = self.gamma.view(1, -1, 1, 1)
         beta = self.beta.view(1, -1, 1, 1)
-
-        x = torch.tanh(self.alpha * x)
         return gamma * x + beta
+
+
+class DyT(nn.Module):
+    def __init__(self, num_features: int, init_alpha: float = 1.0) -> None:
+        super().__init__()
+        self.alpha = nn.Parameter(torch.tensor(init_alpha))
+        self.affine = AffineTransform(num_features)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = torch.tanh(self.alpha * x)
+        return self.affine(x)
 
 
 class DyTV(nn.Module):
     def __init__(self, num_features: int, init_alpha: float = 1.0) -> None:
         super().__init__()
         self.alpha = nn.Parameter(torch.ones(num_features) * init_alpha)
-        self.gamma = nn.Parameter(torch.ones(num_features))
-        self.beta = nn.Parameter(torch.zeros(num_features))
+        self.affine = AffineTransform(num_features)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         alpha = self.alpha.view(1, -1, 1, 1)
-        gamma = self.gamma.view(1, -1, 1, 1)
-        beta = self.beta.view(1, -1, 1, 1)
-
         x = torch.tanh(alpha * x)
-        return gamma * x + beta
-
-
-class DyTVMC(nn.Module):
-    def __init__(self, num_features: int, init_alpha: float = 1.0) -> None:
-        super().__init__()
-        self.alpha = nn.Parameter(torch.ones(num_features) * init_alpha)
-        self.gamma = nn.Parameter(torch.ones(num_features))
-        self.beta = nn.Parameter(torch.zeros(num_features))
-        self.mu = nn.Parameter(torch.zeros(num_features))
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        alpha = self.alpha.view(1, -1, 1, 1)
-        gamma = self.gamma.view(1, -1, 1, 1)
-        beta = self.beta.view(1, -1, 1, 1)
-        mu = self.mu.view(1, -1, 1, 1)
-
-        x = torch.tanh(alpha * (x - mu))
-        return gamma * x + beta
+        return self.affine(x)
 
 
 class DyAS(nn.Module):
     def __init__(self, num_features: int, init_alpha: float = 1.0) -> None:
         super().__init__()
+        self.alpha = nn.Parameter(torch.tensor(init_alpha))
+        self.affine = AffineTransform(num_features)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = x / torch.sqrt(1 + (self.alpha * x) ** 2)
+        return self.affine(x)
+
+
+class DyASV(nn.Module):
+    def __init__(self, num_features: int, init_alpha: float = 1.0) -> None:
+        super().__init__()
         self.alpha = nn.Parameter(torch.ones(num_features) * init_alpha)
-        self.gamma = nn.Parameter(torch.ones(num_features))
-        self.beta = nn.Parameter(torch.zeros(num_features))
-        self.mu = nn.Parameter(torch.zeros(num_features))
+        self.affine = AffineTransform(num_features)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         alpha = self.alpha.view(1, -1, 1, 1)
-        gamma = self.gamma.view(1, -1, 1, 1)
-        beta = self.beta.view(1, -1, 1, 1)
-        mu = self.mu.view(1, -1, 1, 1)
+        x = x / torch.sqrt(1 + (alpha * x) ** 2)
+        return self.affine(x)
 
-        x = alpha * (x - mu)
-        x = x / torch.sqrt(1 + x ** 2)
-        return gamma * x + beta
+
+class DyS(nn.Module):
+    def __init__(self, num_features: int, init_alpha: float = 1.0) -> None:
+        super().__init__()
+        self.alpha = nn.Parameter(torch.tensor(init_alpha))
+        self.affine = AffineTransform(num_features)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = torch.softsign(self.alpha * x)
+        return self.affine(x)
+
+
+class DySV(nn.Module):
+    def __init__(self, num_features: int, init_alpha: float = 1.0) -> None:
+        super().__init__()
+        self.alpha = nn.Parameter(torch.ones(num_features) * init_alpha)
+        self.affine = AffineTransform(num_features)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        alpha = self.alpha.view(1, -1, 1, 1)
+        x = torch.softsign(alpha * x)
+        return self.affine(x)
 
 
 def get_norm_layer(norm_type: str) -> Callable[[int], nn.Module]:
@@ -79,9 +92,13 @@ def get_norm_layer(norm_type: str) -> Callable[[int], nn.Module]:
         return DyT
     elif norm_type == 'dytv':
         return DyTV
-    elif norm_type == 'dytvmc':
-        return DyTVMC
     elif norm_type == 'dyas':
         return DyAS
+    elif norm_type == 'dyasv':
+        return DyASV
+    elif norm_type == 'dys':
+        return DyS
+    elif norm_type == 'dysv':
+        return DySV
     else:
         raise ValueError(f'Unknown normalization type {norm_type}.')
